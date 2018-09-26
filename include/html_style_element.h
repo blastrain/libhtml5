edef073d41473e0af292f6b869e495478b1853ff
kanataxa <sota.corei324@gmail.com>
2018-09-26 19:26:53 +0900

    refactor

--- include/html_style_element.h
+++ include/html_style_element.h
@@ -9,14 +9,14 @@ NAMESPACE_HTML5_BEGIN;
 class HTMLStyleElement : public HTMLElement, public LinkStyle {
 public:
 
+    HTML5_PROPERTY(HTMLStyleElement, std::string, media);
+    HTML5_PROPERTY(HTMLStyleElement, bool, scoped);
+    HTML5_VIRTUAL_READONLY_PROPERTY_OBJECT(HTMLStyleElement, StyleSheet, sheet);
+    HTML5_PROPERTY(HTMLStyleElement, std::string, type);
+
     HTMLStyleElement(emscripten::val v);
     virtual ~HTMLStyleElement();
     static HTMLStyleElement *create(emscripten::val v);
-
-    HTML5_PROPERTY(HTMLStyleElement, std::string, media);
-    HTML5_PROPERTY(HTMLStyleElement, bool, scoped);
-    HTML5_VIRTUAL_PROPERTY_OBJECT(HTMLStyleElement, StyleSheet, sheet);
-    HTML5_PROPERTY(HTMLStyleElement, std::string, type);
 };
 
 NAMESPACE_HTML5_END;
--- include/link_style.h
+++ include/link_style.h
@@ -8,8 +8,10 @@ NAMESPACE_HTML5_BEGIN;
 
 class LinkStyle {
 public:
+
+    HTML5_PURE_VIRTUAL_READONLY_PROPERTY_OBJECT(LinkStyle, StyleSheet, sheet);
+
     virtual ~LinkStyle();
-    HTML5_PURE_VIRTUAL_PROPERTY_OBJECT(LinkStyle, StyleSheet, sheet);
 };
 
 NAMESPACE_HTML5_END;

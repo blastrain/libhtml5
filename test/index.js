87188307ae6c05fbda505c4ca978617ce43b31d3
kanataxa <sota.corei324@gmail.com>
2018-10-11 16:36:58 +0900

    update puppetter test code

--- test/index.js
+++ test/index.js
@@ -1,12 +1,27 @@
 const puppeteer = require('puppeteer');
 
-puppeteer.launch().then(async browser => {
-	  const page = await browser.newPage();
-	  let currentURL;
-	  page.mainFrame()
-	    .waitForXPath('//img')
-	    .then(() => console.log('First URL with image: ' + currentURL));
-	  for (currentURL of ['https://example.com', 'https://google.com', 'https://bbc.com'])
-		    await page.goto(currentURL);
-	  await browser.close();
+puppeteer.launch({
+	/**
+	 * if you want to see console log in browser javascript, enable options.
+	 * devtools: true,
+ 	*/
+}).then(async browser => {
+	const page = await browser.newPage();
+	await page.addScriptTag({
+		path: '../examples/build/libhtml5_example.js',
+	});
+
+	await page.on('console', async msg => {
+		console.log(msg.text());
+	});
+
+	await page.evaluate(() => {
+		Module.executeTest();
+        return Promise.resolve();
+	}).catch(e => {
+	  	console.error(e);
+	  	return Promise.resolve(e);
+	});
+
+	await browser.close();
 });

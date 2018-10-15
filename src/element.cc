d548888254b7ddc44ef4b3c9bd65107204c98b64
kanataxa <sota.corei324@gmail.com>
2018-10-15 19:35:37 +0900

    add HTMLBodyElement

--- src/element.cc
+++ src/element.cc
@@ -3,6 +3,7 @@
 #include "html_collection.h"
 #include "node_list.h"
 #include "element.h"
+#include "html_body_element.h"
 #include "html_div_element.h"
 #include "html_element.h"
 #include "html_image_element.h"
@@ -14,6 +15,7 @@
 USING_NAMESPACE_HTML5;
 
 HTML5_CLASS_FACTORY(Element) {
+    HTML5_SUBCLASS_FACTORY(HTMLBodyElement),
     HTML5_SUBCLASS_FACTORY(HTMLDivElement),
     HTML5_SUBCLASS_FACTORY(HTMLElement),
     HTML5_SUBCLASS_FACTORY(HTMLImageElement),

ec61e28e1d6d1e0291db31f5e3444fcb334a72a8
Masaaki Goshima <goccy54@gmail.com>
2018-10-18 13:39:13 +0900

    add Array methods

--- include/array.h
+++ include/array.h
@@ -15,7 +15,7 @@ public:
         emscripten::val v;
         size_t index;
 
-        Element(emscripten::val v, size_t index);
+        Element(emscripten::val v, size_t index = 0);
         ~Element();
         void operator=(int value);
         void operator=(double value);
@@ -60,19 +60,63 @@ public:
     Array(emscripten::val v);
     virtual ~Array();
     static Array *create(emscripten::val v);
-    static Array *create(unsigned long length);
+    static Array *create(unsigned long length = 0);
     Element operator[](std::size_t index) const;
     Element operator[](std::size_t index);
     void forEach(std::function<void(const Element &elem)> callback);
     void forEach(std::function<void(const Element &elem, int index)> callback);
     void forEach(std::function<void(const Element &elem, int index, Array *array)> callback);
     template<typename... Args> unsigned long push(const Args&... args) {
-        return HTML5_CALL(this->v, push, to_val(args)...);
+        return HTML5_CALLi(this->v, push, unsigned long, to_val(args)...);
     };
-    template<typename T> T pop();
-
+    Element pop();
+    Array *concat(Array *array);
+    bool includes(int searchElement, int fromIndex = 0);
+    bool includes(double searchElement, int fromIndex = 0);
+    bool includes(std::string searchElement, int fromIndex = 0);
+    std::string join(std::string separator = ",");
+    Array *slice(int begin = 0);
+    Array *slice(int begin, int end);
+    int indexOf(int searchElement, int fromIndex = 0);
+    int indexOf(double searchElement, int fromIndex = 0);
+    int indexOf(std::string searchElement, int fromIndex = 0);
+    int lastIndexOf(int searchElement, int fromIndex = 0);
+    int lastIndexOf(double searchElement, int fromIndex = 0);
+    int lastIndexOf(std::string searchElement, int fromIndex = 0);
+    bool every(std::function<bool(const Element &elem)> callback);
+    bool some(std::function<bool(const Element &elem)> callback);
+    Array *filter(std::function<bool(const Element &elem)> callback);
+    Element find(std::function<bool(const Element &elem)> callback);
+    int findIndex(std::function<bool(const Element &elem)> callback);
+    Array *map(std::function<Element(const Element &elem)> callback);
+    Element reduce(std::function<Element(Element,Element)> callback);
+    Element reduce(std::function<Element(Element,Element,int)> callback);
+    Element reduce(std::function<Element(Element,Element,int,Array*)> callback);
+    Element reduceRight(std::function<Element(Element,Element)> callback);
+    Element reduceRight(std::function<Element(Element,Element,int)> callback);
+    Element reduceRight(std::function<Element(Element,Element,int,Array*)> callback);
+    void fill(int value, int start = 0);
+    void fill(int value, int start, int end);
+    void fill(double value, int start = 0);
+    void fill(double value, int start, int end);
+    void fill(std::string value, int start = 0);
+    void fill(std::string value, int start, int end);
+    Array *reverse();
+    Element shift();
+    Array *sort();
+    Array *sort(std::function<int(const Element &a, const Element &b)> compareFn);
+    Array *splice(int index);
+    Array *splice(int index, unsigned long howMany);
+    template<typename... Args> Array *splice(int index, unsigned long howMany, const Args&... args) {
+        return Array::create(HTML5_CALL(this->v, splice, index, howMany, to_val(args)...));
+    };
+    template<typename... Args> unsigned long unshift(const Args& ...args) {
+        return HTML5_CALLi(this->v, unshift, unsigned long, to_val(args)...);
+    };
+    
 private:
     emscripten::val to_val(Object *o) { return o->v; };
+    emscripten::val to_val(const Element &o) { return o.v; };
     emscripten::val to_val(std::string o) {
 #if ENABLE_EMSCRIPTEN
         return emscripten::val(o);
--- src/array.cc
+++ src/array.cc
@@ -88,20 +88,235 @@ void Array::forEach(std::function<void(const Array::Element &elem, int index, Ar
     }
 }
 
-template<typename T> T toVal(Object *o, emscripten::val v)
+Array::Element Array::pop()
 {
-    return dynamic_cast<T>(Object::create(v));
+    return Array::Element(HTML5_CALLv(this->v, pop));
 }
 
-template<typename T> T toVal(T i, emscripten::val v)
+Array *Array::concat(Array *array)
 {
-    return v.as<T>();
+    return Array::create(HTML5_CALLv(this->v, concat, array->v));
 }
 
-template<typename T> T Array::pop()
+bool Array::includes(int searchElement, int fromIndex)
 {
-    T tmp;
-    return toVal<T>(tmp, HTML5_CALLv(this->v, pop));
+    return HTML5_CALLb(this->v, includes, searchElement, fromIndex);
+}
+
+bool Array::includes(double searchElement, int fromIndex)
+{
+    return HTML5_CALLb(this->v, includes, searchElement, fromIndex);
+}
+
+bool Array::includes(std::string searchElement, int fromIndex)
+{
+    return HTML5_CALLb(this->v, includes, searchElement, fromIndex);
+}
+
+std::string Array::join(std::string separator)
+{
+    return HTML5_CALLs(this->v, join, separator);
+}
+
+Array *Array::slice(int begin)
+{
+    return Array::create(HTML5_CALLv(this->v, slice, begin));
+}
+
+Array *Array::slice(int begin, int end)
+{
+    return Array::create(HTML5_CALLv(this->v, slice, begin, end));
+}
+
+int Array::indexOf(int searchElement, int fromIndex)
+{
+    return HTML5_CALLi(this->v, indexOf, int, searchElement, fromIndex);
+}
+
+int Array::indexOf(double searchElement, int fromIndex)
+{
+    return HTML5_CALLi(this->v, indexOf, int, searchElement, fromIndex);
+}
+
+int Array::indexOf(std::string searchElement, int fromIndex)
+{
+    return HTML5_CALLi(this->v, indexOf, int, searchElement, fromIndex);
+}
+
+int Array::lastIndexOf(int searchElement, int fromIndex)
+{
+    return HTML5_CALLi(this->v, lastIndexOf, int, searchElement, fromIndex);
+}
+
+int Array::lastIndexOf(double searchElement, int fromIndex)
+{
+    return HTML5_CALLi(this->v, lastIndexOf, int, searchElement, fromIndex);
+}
+
+int Array::lastIndexOf(std::string searchElement, int fromIndex)
+{
+    return HTML5_CALLi(this->v, lastIndexOf, int, searchElement, fromIndex);
+}
+
+bool Array::every(std::function<bool(const Element &elem)> callback)
+{
+    for (size_t i = 0; i < this->v["length"].as<unsigned long>(); i++) {
+        if (!callback((*this)[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+bool Array::some(std::function<bool(const Element &elem)> callback)
+{
+    for (size_t i = 0; i < this->v["length"].as<unsigned long>(); i++) {
+        if (callback((*this)[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+Array *Array::filter(std::function<bool(const Element &elem)> callback)
+{
+    Array *newArray = Array::create();
+    for (size_t i = 0; i < this->v["length"].as<unsigned long>(); i++) {
+        if (callback((*this)[i])) {
+            newArray->push((*this)[i]);
+        }
+    }
+    return newArray;
+}
+
+Array::Element Array::find(std::function<bool(const Element &elem)> callback)
+{
+    for (size_t i = 0; i < this->v["length"].as<unsigned long>(); i++) {
+        if (callback((*this)[i])) {
+            return (*this)[i];
+        }
+    }
+#if ENABLE_EMSCRIPTEN
+    return Array::Element(emscripten::val::undefined());
+#else
+    return Array::Element(emscripten::val(0));
+#endif
+}
+
+int Array::findIndex(std::function<bool(const Element &elem)> callback)
+{
+    for (size_t i = 0; i < this->v["length"].as<unsigned long>(); i++) {
+        if (callback((*this)[i])) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+Array *Array::map(std::function<Element(const Element &elem)> callback)
+{
+    Array *newArray = Array::create();
+    for (size_t i = 0; i < this->v["length"].as<unsigned long>(); i++) {
+        newArray->push(callback((*this)[i]));
+    }
+    return newArray;
+}
+
+Array::Element Array::reduce(std::function<Element(Element,Element)> callback)
+{
+    // TODO
+    return Array::Element(emscripten::val(0));
+}
+
+Array::Element Array::reduce(std::function<Element(Element,Element,int)> callback)
+{
+    // TODO
+    return Array::Element(emscripten::val(0));
+}
+
+Array::Element Array::reduce(std::function<Element(Element,Element,int,Array*)> callback)
+{
+    // TODO
+    return Array::Element(emscripten::val(0));
+}
+
+Array::Element Array::reduceRight(std::function<Element(Element,Element)> callback)
+{
+    // TODO
+    return Array::Element(emscripten::val(0));
+}
+
+Array::Element Array::reduceRight(std::function<Element(Element,Element,int)> callback)
+{
+    // TODO
+    return Array::Element(emscripten::val(0));
+}
+
+Array::Element Array::reduceRight(std::function<Element(Element,Element,int,Array*)> callback)
+{
+    // TODO
+    return Array::Element(emscripten::val(0));
+}
+
+void Array::fill(int value, int start)
+{
+    HTML5_CALL(this->v, fill, value, start);
+}
+
+void Array::fill(int value, int start, int end)
+{
+    HTML5_CALL(this->v, fill, value, start, end);
+}
+
+void Array::fill(double value, int start)
+{
+    HTML5_CALL(this->v, fill, value, start);
+}
+
+void Array::fill(double value, int start, int end)
+{
+    HTML5_CALL(this->v, fill, value, start, end);
+}
+
+void Array::fill(std::string value, int start)
+{
+    HTML5_CALL(this->v, fill, value, start);
+}
+
+void Array::fill(std::string value, int start, int end)
+{
+    HTML5_CALL(this->v, fill, value, start, end);
+}
+
+Array *Array::reverse()
+{
+    return Array::create(HTML5_CALLv(this->v, reverse));
+}
+
+Array::Element Array::shift()
+{
+    return Array::Element(HTML5_CALLv(this->v, shift));
+}
+
+Array *Array::sort()
+{
+    return Array::create(HTML5_CALLv(this->v, sort));
+}
+
+Array *Array::sort(std::function<int(const Element &a, const Element &b)> compareFn)
+{
+    // TODO
+    return Array::create();
+}
+
+Array *Array::splice(int index)
+{
+    return Array::create(HTML5_CALLv(this->v, splice, index));
+}
+
+Array *Array::splice(int index, unsigned long howMany)
+{
+    return Array::create(HTML5_CALLv(this->v, splice, index, howMany));
 }
 
 HTML5_PROPERTY_IMPL(Array, unsigned long, length);
